057aed54fb58ba5e3d602f56900e58ccb625ad3d
Barwoz.Dev <85193395+Barwoz@users.noreply.github.com>
2021-09-20 20:03:19 +0200

    Add files via upload

---
+++ barwoz_armurerie/armurerie.sql
@@ -0,0 +1,4 @@
+INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES
+('flashlight', 'Lampe', 1, 0, 1),
+('silencieux', 'Silencieux', 1, 0, 1),
+('yusuf', 'Skin de luxe', 1, 0, 1);
